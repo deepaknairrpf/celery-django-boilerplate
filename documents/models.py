244ff37c739a04264812f6a7484a3f8384c5bf7e
dingusagar <dingusagar@gmail.com>
2018-11-02 13:07:41 +0530

    created Documents Model

--- documents/models.py
+++ documents/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Documents(models.Model):
+    title = models.CharField(max_length=150)
+    document_url = models.ImageField(null=False,blank=False)
+    # owner = models.ForeignKey()   ## foreign key to user id
+    # organisation_id = models.ForeignKey()  ## foreign key to organisations
